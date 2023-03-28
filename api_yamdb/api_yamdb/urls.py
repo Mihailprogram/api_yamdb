fbddd390c59e45a911a98c37925f460269c29c1b
Михаил Химач <miha.vjk@mail.ru>
2023-03-28 15:59:59 +0500

    fix url

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.urls import include, path
+from rest_framework import routers
+from titles.views import TitleViewSet, GenreViewSet, CategoryViewSet
 
+router = routers.DefaultRouter()
+router.register('titles', TitleViewSet)
+router.register('genres', GenreViewSet)
+router.register('categories', CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
     path('api/', include('reviews.urls')),
     path(
         'redoc/',
